0c93af1560c1e54ecafc1c1b9c697fb7e8487b77
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-04-24 21:47:28 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o 1° número: "))
+n2 = int(input("Digite o 2° número: "))
+
+if n1 > n2:
+    print(n2, "é o menor número")
+else:
+    print(n1, "é o menor número")
+
+input("Digite qualquer tecla para sair do aplicativo...")
