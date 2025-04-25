0c8d542f5cfef49f4ce39af3a90e5952df75ef42
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-04-25 15:02:16 -0300

    Create ex07.py

---
+++ ex07.py
@@ -0,0 +1,25 @@
+print("1\tMédia entre os dois números")
+print("2\tDiferença do maior pelo menor")
+print("3\tO produto entre os dois números")
+
+escolha = int(input("\nEscolha a operação desejada: "))
+
+if escolha == 1:
+    n1 = float(input("\nDigite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+    print(f"A média entre {n1} e {n2} é: {(n1 + n2) / 2:.2f}")
+    input("\nPressione qualquer tecla para fechar o programa...")
+elif escolha == 2:
+    n1 = float(input("\nDigite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+    if n1 > n2:
+        print(f"A diferença entre {n1} e {n2} é: {n1 - n2}")
+    else: print(f"A diferença entre {n2} e {n1} é: {n2- n1}")
+    input("\nPressione qualquer tecla para fechar o programa...")
+elif escolha == 3:
+    n1 = float(input("\nDigite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+    print(f"O produto entre {n1} e {n2} é: {n1 * n2:.2f}")
+    input("\nPressione qualquer tecla para fechar o programa...")
+else: 
+    print("Opção inválida!")
