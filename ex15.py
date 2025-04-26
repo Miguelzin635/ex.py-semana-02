187191e642249a594f48671306cab912727a2bbb
MiguelP <77172963+Miguelzin635@users.noreply.github.com>
2025-04-26 18:27:44 -0300

    Create ex15.py

---
+++ ex15.py
@@ -0,0 +1,8 @@
+senha = str(input("Digite sua senha:"))
+senha_correta = str("A4533BC")
+if senha == senha_correta:
+    print("Acesso permitido.")
+else: 
+    print("Acesso negado.")
+
+input("Pressione Enter para fechar o programa...")
